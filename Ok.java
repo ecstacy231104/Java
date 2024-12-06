c9b55422a9bf68c3fc968a4ad9554f1e00f6f3df
Ritika <ritika23.bhaumik@gmail.com>
2024-12-06 16:44:20 +0530

    scanner code added

---
+++ Ok.java
@@ -0,0 +1,8 @@
+public class Ok {
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+        
+    }
+    
+}
---
+++ userinput/Userinput.java
@@ -0,0 +1,17 @@
+package userinput;
+import java.util.Scanner;
+public class Userinput {
+    public static void main(String[] args)
+    {
+        int x;
+        System.out.println("enter a value for x =");
+        Scanner scanobj=new Scanner(System.in);
+        x=scanobj.nextInt();
+        System.out.println("the value of x i "+x);
+        
+
+    }
+
+    
+}
+
